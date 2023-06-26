1ae6485a99a337af4f78b7a3fe60a9fb961fdbe3
Breaddcrumbss <lukechong2898.lc@gmail.com>
2023-06-26 19:30:15 +0800

    first commit

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload', views.upload, name='upload'),
+    path('generate', views.generate, name='generate')
+
+
+]
